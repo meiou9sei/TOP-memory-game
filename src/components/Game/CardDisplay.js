63e9d09a8d1dd0e37fbee6aa712f9314a5b84587
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-27 18:12:11 -0500

    ✨ create function to update clicked state after click, display win/loss in console

--- src/components/Game/CardDisplay.js
+++ src/components/Game/CardDisplay.js
@@ -2,7 +2,24 @@ import { Cards } from "./Cards";
 import { useState } from "react";
 
 export default function CardDisplay() {
-  const [cardsArray, setCardsArray] = useState(randomize(Cards));
+  const [cardsArray, setCardsArray] = useState(Cards);
+
+  function shuffleCards(cards, cardId) {
+    const cardToCheck = cardsArray.find((card) => card.id === cardId);
+    if (cardToCheck.clicked) {
+      console.log("you lose!");
+    } else {
+      console.log("you live this time...");
+      // update card clicked boolean
+      const newState = cardsArray.map((card) => {
+        if (card.id === cardId) {
+          return { ...card, clicked: true };
+        }
+        return card;
+      });
+      setCardsArray(newState);
+    }
+  }
 
   function randomize(array) {
     return array
@@ -13,11 +30,11 @@ export default function CardDisplay() {
 
   return (
     <div className='card-display'>
-      {cardsArray.map((card) => (
+      {randomize(cardsArray).map((card) => (
         <div
           key={card.id}
           className='card'
-          onClick={() => setCardsArray(randomize(Cards))}
+          onClick={() => shuffleCards(Cards, card.id)}
         >
           <span>{card.content}</span>
         </div>
