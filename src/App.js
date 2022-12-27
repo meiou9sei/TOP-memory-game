34a7e0d6be5ec98c8f07e150e6293ab187f83287
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-26 19:45:46 -0500

    🚧 create card display component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Footer from "./components/Footer";
+import CardDisplay from "./components/Game/CardDisplay";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      Hello World!
+      <main>
+        <CardDisplay />
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/Game/CardDisplay.js
@@ -0,0 +1,3 @@
+export default function CardDisplay() {
+  return <div>test</div>;
+}
