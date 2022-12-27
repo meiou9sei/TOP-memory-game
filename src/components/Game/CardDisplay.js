af98b9ffabb7ea60929dae20ffdab75639c2144a
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-12-27 17:29:10 -0500

    ✨ cards randomize upon click

--- src/components/Game/CardDisplay.js
+++ src/components/Game/CardDisplay.js
@@ -1,10 +1,24 @@
 import { Cards } from "./Cards";
+import { useState } from "react";
 
 export default function CardDisplay() {
+  const [cardsArray, setCardsArray] = useState(randomize(Cards));
+
+  function randomize(array) {
+    return array
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+  }
+
   return (
     <div className='card-display'>
-      {Cards.map((card) => (
-        <div key={card.id} className='card'>
+      {cardsArray.map((card) => (
+        <div
+          key={card.id}
+          className='card'
+          onClick={() => setCardsArray(randomize(Cards))}
+        >
           {card.content}
         </div>
       ))}
